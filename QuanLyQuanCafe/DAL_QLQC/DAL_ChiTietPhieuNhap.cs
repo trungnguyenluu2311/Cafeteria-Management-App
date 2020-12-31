b013bdc0358cbe60571a23f0a084be730fa2fbd4
ndmnhat <nhatvn2000@gmail.com>
2021-01-01 00:39:12 +0700

    Update dbscript

--- QuanLyQuanCafe/DAL_QLQC/DAL_ChiTietPhieuNhap.cs
+++ QuanLyQuanCafe/DAL_QLQC/DAL_ChiTietPhieuNhap.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks
+using System.Threading.Tasks;
 using System.Data;
 using System.Windows.Forms;
 using QuanLyQuanCafe.DTO_QCQC;
